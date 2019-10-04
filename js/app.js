b907e3f059d98fa43b1933a723c8287815f76217
Julia Fong <juliafong@Julias-MacBook-Pro.local>
2019-10-04 13:35:08 -0700

    round two answers

--- js/app.js
+++ js/app.js
@@ -8,17 +8,25 @@ const questionThree = ['Ragtimne', ' Showboat', ' Oklahoma!', ' Sound of Music',
 
 const questionFour = ['My Fair Lady', ' Sound of Music', ' The Jazz Singer', ' Grease'];
 
-const questionFive = ['Donal O,Connor', 'Debbie Reynolds', 'Stanley Donen', 'Gene Kelly', ];
-
+const questionFive = ['Donald O,Connor', 'Debbie Reynolds', 'Stanley Donen', 'Gene Kelly', ];
 console.log(questionFive);
 console.log(questionFour);
 console.log(questionThree);
 console.log(questionTwo);
 console.log(questionOne);
+//round two
+const questionSix = ['The Kardashians', ' The Kennedys', ' The Jackson 5', ' The Obamas'];
+const questionSeven = ['Gene Kelly', ' Stephen Sondheim', ' Savion Glover', ' Bob Fosse'];
+const questionEight = ['Sunday in the park with George', ' Sweeney Todd', ' Into the woods', 'Little shop of horrors'];
+const questionNine = [];
+const questionTen = [];
+
 //player one
-const playerOne {
-        name: "Player one",
-        score: 0,
-    }
-    //game
-const
+
+const playerOne = {
+    name: [],
+    age: 0,
+    score: 0,
+    overallScore: 0,
+
+}
