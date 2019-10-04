cf4beb9c3ecb5777d76f6ad279c288a1b152847b
Julia Fong <juliafong@Julias-MacBook-Pro.local>
2019-10-04 10:54:51 -0700

    round one questions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 const questionOne = ['Andrew Lloyd Weber', ' Sir Andrew Lloyd Weber', ' Rogers & Hammerstien', ' Bob Fosse'];
+
+const questionTwo = ['Black Crook', ' Showboat', ' Oklahoma', ' South Pacific', ];
+
+const questionThree = ['Ragtimne', ' Showboat', ' Oklahoma!', ' Sound of Music', ]
+
+const questionFour = ['My Fair Lady', ' Sound of Music', ' The Jazz Singer', ' Grease'];
+
+const questionFive = ['Donal O,Connor', 'Debbie Reynolds', 'Stanley Donen', 'Gene Kelly', ];
