36cbef581df8a66dbbe711979353ca8e2d773075
Julia Fong <juliafong@Julias-MacBook-Pro.local>
2019-10-04 11:09:24 -0700

    making the game skeleton

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//round one
+
 const questionOne = ['Andrew Lloyd Weber', ' Sir Andrew Lloyd Weber', ' Rogers & Hammerstien', ' Bob Fosse'];
 
 const questionTwo = ['Black Crook', ' Showboat', ' Oklahoma', ' South Pacific', ];
@@ -7,3 +9,16 @@ const questionThree = ['Ragtimne', ' Showboat', ' Oklahoma!', ' Sound of Music',
 const questionFour = ['My Fair Lady', ' Sound of Music', ' The Jazz Singer', ' Grease'];
 
 const questionFive = ['Donal O,Connor', 'Debbie Reynolds', 'Stanley Donen', 'Gene Kelly', ];
+
+console.log(questionFive);
+console.log(questionFour);
+console.log(questionThree);
+console.log(questionTwo);
+console.log(questionOne);
+//player one
+const playerOne {
+        name: "Player one",
+        score: 0,
+    }
+    //game
+const
